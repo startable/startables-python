70ceb5b02e52f08814cc22a2ba0d050f476410d4
JEACO <jeaco@orsted.dk>
2019-10-17 21:33:41 +0200

    Add tests for glob and list of files

--- startables/test/test_bulk.py
+++ startables/test/test_bulk.py
@@ -16,8 +16,24 @@ def bulk_dir(input_dir) -> Path:
     return input_dir / 'read_bulk'
 
 
-def test_read_bulk(bulk_dir):
+def test_read_bulk_dir(bulk_dir):
     bundle = read_bulk(bulk_dir)
+    assert bundle  # is not None
+    assert len(bundle.tables) == 3
+    assert len(bundle.filter(name_pattern='BarTable').tables) == 1
+    assert len(bundle.filter(name_pattern='BazBass').tables) == 2
+
+
+def test_read_bulk_glob(bulk_dir):
+    bundle = read_bulk(str(bulk_dir / '*'))
+    assert bundle  # is not None
+    assert len(bundle.tables) == 3
+    assert len(bundle.filter(name_pattern='BarTable').tables) == 1
+    assert len(bundle.filter(name_pattern='BazBass').tables) == 2
+
+
+def test_read_bulk_files(bulk_dir):
+    bundle = read_bulk([bulk_dir / 'simple_bar.csv', bulk_dir / 'simple_baz_with2sheets.xlsx'])
     assert bundle  # is not None
     assert len(bundle.tables) == 3
     assert len(bundle.filter(name_pattern='BarTable').tables) == 1
